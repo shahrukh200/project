0bf183aade7c8f81fd555a3682f827241c45d71f
shahrukh200 <mohamedshahrukh618@gmail.com>
2024-10-22 11:34:53 +0530

    github

--- spoke1/locals.tf
+++ spoke1/locals.tf
@@ -1,7 +1,7 @@
 locals {
   subnet_names =[for  sub in azurerm_subnet.subnet:sub.name]
   nsg_names = {for i,nsg in azurerm_network_security_group.nsg:i=>nsg.name}
-  subnet_id = [for i in azurerm_subnet.subnet : i.id]
+  subnet_id = [for i in azurerm_subnet.subnet :i.id]
   rules_csv=csvdecode(file(var.rule_file))
 
 
